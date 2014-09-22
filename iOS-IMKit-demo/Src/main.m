b7dcdda47355dc0ce9c348981e7e0f08a3b2992d
HeqShinoda <hequn@feinno.com>
2014-09-22 19:42:45 +0800

    － Optimaz.

--- iOS-IMKit-demo/Src/main.m
+++ iOS-IMKit-demo/Src/main.m
@@ -10,24 +10,9 @@
 
 #import "AppDelegate.h"
 
-#ifdef DEBUG
-#undef LOGTOFILE
-#else
-#define LOGTOFILE
-#endif
 
 int main(int argc, char *argv[])
 {
-#ifdef LOGTOFILE
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentDir = [paths objectAtIndex: 0];
-    NSString *logPath = [documentDir stringByAppendingPathComponent:@"xsz.log"];
-    NSLog(@"%@", logPath);
-    freopen([logPath cStringUsingEncoding: NSASCIIStringEncoding], "a+", stdout);
-    
-    NSString *logPath2 = [documentDir stringByAppendingPathComponent:@"xsz2.log"];
-    freopen([logPath2 cStringUsingEncoding: NSASCIIStringEncoding], "a+", stderr);
-#endif
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
